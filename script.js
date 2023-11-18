ce65b131979b2df37a89115f4ed8f20afbaec992
EmreTekeli <103356866+EmreTekeli@users.noreply.github.com>
2023-11-18 17:40:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+function generateRandomString(length) {
+  const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return Array.from(values)
+    .map((value) => charset[value % charset.length])
+    .join('');
+}
+
 function generateCountdownLink() {
   const countdownDate = new Date(document.getElementById('countdownDate').value).getTime();
   const message = document.getElementById('messageInput').value;
   const encodedMessage = encodeURIComponent(message);
 
+  // localStorage kullanarak mesajı sakla
+  localStorage.setItem('countdownDate', countdownDate);
+  localStorage.setItem('message', encodedMessage);
+
   const baseURL = 'https://emretekeli.github.io/MesajBirak.github.io/preview.html'; // Sabit URL
 
+  const storedCountdownDate = localStorage.getItem('countdownDate');
+  const storedMessage = localStorage.getItem('message');
+
   const queryParams = new URLSearchParams();
-  queryParams.set('countdownDate', countdownDate);
-  queryParams.set('message', encodedMessage);
+  queryParams.set('unique', generateRandomString(8)); // Benzersiz parametre
+  queryParams.set('countdownDate', storedCountdownDate);
+  queryParams.set('message', storedMessage);
 
   const link = `${baseURL}?${queryParams.toString()}`; // URL'yi oluştur
 
@@ -30,4 +46,7 @@ function generateCountdownLink() {
   };
 
   generatedLink.appendChild(shareButton);
+
+  // Oluşturulan linki konsola yazdırma
+  console.log(link);
 }
