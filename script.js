d4a563a2ab42f110a2068a99639c540a1ccbc026
EmreTekeli <103356866+EmreTekeli@users.noreply.github.com>
2023-11-18 10:56:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function generateCountdownLink() {
-    const countdownDate = new Date(document.getElementById('countdownDate').value).getTime();
-    const message = document.getElementById('messageInput').value;
+  const countdownDate = new Date(document.getElementById('countdownDate').value).getTime();
+  const message = document.getElementById('messageInput').value;
+  const encodedMessage = encodeURIComponent(message);
+
+  // localStorage kullanarak mesajı sakla
+  localStorage.setItem('countdownDate', countdownDate);
+  localStorage.setItem('message', encodedMessage);
+
+  const link = `preview.html`;
   
-    const link = `preview.html?date=${countdownDate}`;
-    
-    document.getElementById('generatedLink').innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
-  }
-  
+  document.getElementById('generatedLink').innerHTML = `<a href="${link}" target="_blank">${link}</a>`;
+}
